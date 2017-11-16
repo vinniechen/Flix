df497a8f2c92a8c8a7f07ccac950a464a7d6e786
Vinnie Chen <vinnie.chen211@gmail.com>
2017-11-15 22:14:11 -0800

    Added poster image, pull down refresh, activity loading state

--- Flicks/NowPlayingViewController.swift
+++ Flicks/NowPlayingViewController.swift
@@ -7,32 +7,29 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
     var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    let baseURLString = "https://image.tmdb.org/t/p/w500"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.startAnimating()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         
         tableView.dataSource = self
         tableView.delegate = self
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-        let request =  URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-                let moviesDictionary = dataDictionary["results"] as! [[String: Any]]
-                self.movies = moviesDictionary
-                self.tableView.reloadData()
-            }
-        }
-        task.resume()
+        fetchMovies()
         
     }
     
@@ -46,7 +43,11 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let poster = movie["poster_path"]
+        let posterPath = movie["poster_path"] as! String
+
+        
+        let posterURL = URL(string: baseURLString + posterPath)!
+        cell.poster.af_setImage(withURL: posterURL)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
@@ -56,6 +57,30 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource, UITable
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return CGFloat(160)
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request =  URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: URLSessionConfiguration.default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let moviesDictionary = dataDictionary["results"] as! [[String: Any]]
+                self.movies = moviesDictionary
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+                self.activityIndicator.stopAnimating()
+            }
+        }
+        task.resume()
+    }
+    
+    @objc func didPullRefresh(_ refreshControl: UIRefreshControl) {
+            fetchMovies()
     }
     
     override func didReceiveMemoryWarning() {
